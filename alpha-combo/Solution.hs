a5912cb2b77072d37b6174ae46d18530cc4adb79
Li Zhaoyang <lizy14@yeah.net>
2016-10-24 16:39:51 +0800

    minor

--- alpha-combo/Solution.hs
+++ alpha-combo/Solution.hs
@@ -1,6 +1,6 @@
 -- alpha-combo
 module Solution where
     import Data.List (subsequences, sort)
-    solution :: Integer -> [[Char]]
     combinations k ns = filter ((k==).length) (subsequences ns)
-    solution i = sort $ combinations (fromIntegral i) "abcdefghijklmnopqrstuvwxyz"
+    solution :: Integer -> [[Char]]
+    solution i = sort $ combinations (fromIntegral i) ['a'..'z']
