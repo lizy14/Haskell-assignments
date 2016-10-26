8ebec302e4b4710cc200e1bc5f5241c7c63b7a6b
Li Zhaoyang <lizy14@yeah.net>
2016-10-26 20:59:23 +0800

    hw04

--- cross-many/Solution.hs
+++ cross-many/Solution.hs
@@ -3,6 +3,24 @@ module Solution where
     swap i (xs, ys) = (
         take i xs ++ [(!!) ys i] ++ drop (i + 1) xs,
         take i ys ++ [(!!) xs i] ++ drop (i + 1) ys)
+
+    longer :: [a] -> [a] -> Bool
+    longer xs ys =
+        if null xs
+            then
+                False
+            else
+                if null ys
+                    then
+                        True
+                    else
+                        longer
+                            (tail xs)
+                            (tail ys)
+
+    minLength :: [a] -> [a] -> Int
+    minLength xs ys = length $ if longer xs ys then ys else xs
+
     helper is (xs, ys) =
         if null is
             then
@@ -11,28 +29,18 @@ module Solution where
                 helper
                     (tail is)
                     (swap (head is) (xs, ys))
+
     solution :: [Int] -> [a] -> [a] -> ([a], [a])
     solution is xs ys =
         helper
             (filter
                 (\x -> x >= 0)
                 (takeWhile
-                    (\x ->
-                        and [
-                            x < length xs,
-                            x < length ys
-                        ]
-                    )
-                    is
-                )
-            )
+                    (\x -> x < minLength xs ys)
+                    is))
             (
-                take
-                    (minimum $ map length [xs, ys])
-                    xs,
-                take
-                    (minimum $ map length [xs, ys])
-                    ys
+                take (minLength xs ys) xs,
+                take (minLength xs ys) ys
             )
 
 {- tests
@@ -41,7 +49,9 @@ module Solution where
 solution [3,17] "haskell" "python"   == ("hashel", "pytkon"),
 solution [-7,0,2] "haskell" "python" == ("patkel", "hyshon"),
 solution [3] "haskell" []            == ([], []),
-solution [0..] "haskell" "python"    == ("python", "haskel")
+solution [0..] "haskell" "python"    == ("python", "haskel"),
+solution [0..] [1,3..] [2,4..10]     == ([2,4,6,8,10],[1,3,5,7,9]),
+solution [0,2..] [1,3..] [2,4..10]   == ([2,3,6,7,10],[1,4,5,8,9])
 ]
 :}
 -}
