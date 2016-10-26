869abb789daee8ab0cb7052ccadf9e6b173753c7
Li Zhaoyang <lizy14@yeah.net>
2016-10-26 21:45:58 +0800

    add test

--- cross-one/Solution.hs
+++ cross-one/Solution.hs
@@ -11,7 +11,8 @@ module Solution where
 solution 3 "haskell" "python"    == ("pytkell","hashon"),
 solution 2 "haskell" []          == ("skell","ha"),
 solution 12 "haskell" "meep"     == ("meep","haskell"),
-solution (-1) "haskell" "python" == ("haskell","python")
+solution (-1) "haskell" "python" == ("haskell","python"),
+(\(x,y)->(take 4 x, take 4 y)) (solution 2 [1..] [13..]) == ([13,14,3,4],[1,2,15,16])
 ]
 :}
 -}
